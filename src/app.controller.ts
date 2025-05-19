f0f04741ecde311cb487c153103c5eba6ca0153d
Hashimi <hashimisharudin.work@gmail.com>
2025-05-20 05:27:51 +0800

    Update Health Check API

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,7 +8,7 @@ import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 export class AppController {
   constructor(private readonly service: AppService) {}
 
-  @Get('/health')
+  @Get('/api/health')
   @HealthCheck()
   @ApiOperation({ summary: 'Check API & Database health status' })
   @ApiResponse({ status: 200, description: 'Health check successful' })
