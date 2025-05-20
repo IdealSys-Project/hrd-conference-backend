46c5097a43ebad3474b3dda0ab98d29df2e20588
Hashimi <hashimisharudin.work@gmail.com>
2025-05-20 11:03:47 +0800

    Update header

--- src/main.ts
+++ src/main.ts
@@ -35,6 +35,8 @@ async function bootstrap() {
       }
     },
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
   // Swagger config
